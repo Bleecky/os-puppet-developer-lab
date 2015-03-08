7c981e10ff2dde22f9826e9a2efbedaa45598ac2
Peter Bleeck <peter.bleeck@gmail.com>
2015-03-08 14:24:50 +0000

    Added puppet.conf and hire.yaml files; updated init.pp to manage files

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,17 @@ class ospuppetdevelopmentlab(
   $message = "Configuring os-puppet-development-lab"
   ) { 
       notify { $message: }
+      
+      file {'hiera.yaml':
+        ensure  => present,
+        path    => '/etc/puppet/hiera.yaml',
+        source  => 'puppet:///modules/ospuppetdevelopmentlab/hiera.yaml'
+        }
+
+      file {'puppet.conf':
+        ensure  => present,
+        path    => '/etc/puppet/puppet.conf',
+        source  => 'puppet:///modules/ospuppetdevelopmentlab/puppet.conf'
+        }
+
     }
