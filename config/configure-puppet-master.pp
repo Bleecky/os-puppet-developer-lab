0909373c5879af50714ba3783fbd1b5dfa51498f
Peter Bleeck <peter.bleeck@os.uk>
2015-03-09 15:04:18 +0000

    Added puppet master configuration manifest

---
+++ config/configure-puppet-master.pp
@@ -0,0 +1,23 @@
+# Configure: os-puppet-developer-lab
+
+$config_dir = '/tmp/os-puppet-developer-lab/files/'
+
+file {'/etc/puppet/puppet.conf':
+  ensure => present,
+  source => "${config_dir}/puppet.conf",
+}
+
+file {'/etc/puppet/hiera.yaml':
+  ensure => present,
+  source => "${config_dir}/hiera.yaml",
+}
+
+file {'/etc/puppet/manifests/nodes.pp'
+  ensure  => present,
+  source  => "${config_dir}/nodes.pp"
+}
+
+file {'/etc/puppet/manifests/site.pp'
+  ensure  => present,
+  source  => "${config_dir}/nodes.pp"
+}
